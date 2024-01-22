259700151881ee25f0fb6f10e74740351a3e4b8e
kbprabowo <kuroceper2@gmail.com>
2024-01-22 23:27:10 +0800

    update content story 11

--- src/stories/story-11.js
+++ src/stories/story-11.js
@@ -2,93 +2,111 @@ const dir = "11";
 export const story11 = {
   index: dir,
   thumbnail: require(`images/stories/${dir}/main.webp`),
-  name: "Ria Febriyanti",
+  name: "Elisabeth Rona",
   title: {
-    en: "Orang Muda untuk Perubahan",
-    id: "Orang Muda untuk Perubahan",
+    en: "Lulus Kuliah Lewat Jalur Program Safe and Fair",
+    id: "Lulus Kuliah Lewat Jalur Program Safe and Fair",
   },
   content: {
     en: (
       <div>
         <p>
-          Apa yang terlintas di pikiran jika mendengar kata anak muda?
-          Organisasi PBB seperti UNESCO dan UNFPA mengartikan pemuda sebagai
-          individu yang memiliki usia antara 15-24 tahun. Hampir setiap orang
-          berpikir bahwa masa-masa menjadi anak muda merupakan masa yang sangat
-          seru. Masa mencari jati diri dengan terus menerus menumpuk keinginan
-          yang entah kapan dapat terkabulkan. Layaknya pemikiran-pemikiran
-          orang-orang tentang anak muda, anak muda saat ini tidak dapat
-          disamakan, ada yang memang memiliki semangat yang tinggi untuk masa
-          depannya, ada juga yang berpasrah diri dengan mengikuti arus hidupnya.
-          Sama seperti halnya anak muda di kota kelahiranku, Batam. Berbagai
-          pandangan tentang anak muda seringkali didengar mulai dari yang
-          positif hingga negatif.
+          Lika-liku perjalanan menulis skripsi biasa dialami kebanyakan
+          mahasiswa tingkat akhir. Menulis skripsi seperti menghadapi raja
+          terakhir menuju wisuda. Termasuk aku yang saat ini sedang berkutat di
+          depan layar laptop ditemani kursor di layar yang berkedip-kedip
+          bersamaan berlalunya detik waktu. Tik&hellip;tik...tik... Kursor itu
+          masih berkedip di akhir paragraf yang tidak aku lanjutkan. Nihil.
+          Tidak ada satupun inspirasi yang muncul di kepalaku. Setiap detik
+          kursor itu muncul di layar, semakin meningkat kecemasanku karena tidak
+          mampu menyelesaikan proposal penelitian ini. Aku merasa terjebak dalam
+          situasi yang membuatku frustasi. Terlebih lagi karena ada harapan
+          besar dari keluargaku untuk aku segera lulus kuliah.
         </p>
         <p>
-          Melihat kondisi anak muda di kota kecil ini terkadang juga cukup
-          memprihatinkan. Mereka membangun lingkungan dengan batasan-batasan
-          standar tertentu, seperti harus menggunakan handphone merek tertentu,
-          baju branded, sepatu bagus dan masih banyak lagi, menurutku yang
-          membuat lingkungan sosial semakin sempit. Tidak cakap bersosialisasi
-          dengan pemikiran bahwa tidak ada yang lebih penting dari urusan diri
-          mereka sendiri. Kenyataannya memang seperti itu, situasi anak muda di
-          kota kecil ini.
-        </p>
-        <blockquote>
-          Sangat menarik menurutku ketika ada sekelompok anak muda yang
-          menghabiskan waktunya dengan membahas masalah sosial yang terjadi saat
-          ini. Terlebih fokus mereka ada pada permasalahan kotanya sendiri.
-        </blockquote>
-        <p>
-          Setiap akhir pekan mereka tanpa beban berkumpul dengan canda tawa yang
-          mengiringi rapat-rapat dengan topik serius seperti isu kekerasan yang
-          selalu saja berhasil menguras emosi.
+          Aku sampaikan kepada dosen pembimbing tentang kebuntuanku menulis
+          bahwa aku merasa asing dengan topik skripsi yang kupilih, hingga
+          ketidaktertarikan aku dengan dunia per-skripsi-an ini. Beliau
+          menyarankanku untuk meneliti tentang hal yang aku sukai, seperti
+          tentang LSM, tempat di mana aku menyibukkan diri, yang juga merupakan
+          sebuah dunia baru bagiku. Seseorang biasanya betah menghabiskan banyak
+          waktunya, jika berkutat dengan hal yang ia sukai, seperti aku katanya,
+          yang berkegiatan di LSM Damar selama lebih dari 1 tahun saat itu.
+          Topik awal penelitianku adalah bagaimana peran negara dalam menangani
+          isu perdagangan perempuan pekerja migran yang menjadi korban Tindak
+          Pidana Perdagangan Orang (TPPO). Topikku kemudian berubah dan yang
+          menjadi objek penelitianku adalah LSM Perkumpulan Damar.
         </p>
         <p>
-          Aku mengenal mereka dengan sangat dekat, bahkan aku pernah bergabung
-          di dalamnya tepatnya saat aku masih duduk di bangku SMA. Komunitas
-          anak muda KOMPAK (Komunitas Anak Muda Anti Perdagangan Orang dan
-          Eksploitasi Seksual) dan RAEKSA (Remaja Anti Eksploitasi Seksual Anak)
-          Namanya. Layaknya adik kakak yang lahir dari rahim yang sama, selalu
-          saja ada hal-hal pemantik debat dan pertengkaran yang sebenarnya
-          membuat mereka lebih dekat. Aku belajar dan bertumbuh bersama kelompok
-          anak muda ini. Jika dilihat jauh ke belakang aku ada bersama mereka
-          untuk berjuang bersama melawan dan mencegah kekerasan di kota kecil
-          ini sudah hampir 4 tahun. Bersama mereka, aku menjadi selalu punya
-          alasan untuk memperbaiki diri, belajar dengan lebih baik, hingga
-          mengerti makna yang besar dari berbagi dalam satu tujuan yang searah,
-          bersama untuk bermanfaat bagi orang lain. Bersama dengan penuh harap
-          dan berani mengambil langkah untuk dapat memberi penerangan di jalan
-          menuju masa depan yang lebih baik.
+          <blockquote>
+            Seperti takdir, beberapa bulan kemudian Sely Fitriyani, Direktur
+            Eksekutif Lada Damar, mengundangku hadir dalam Zoom meeting program
+            Safe and Fair dari UN Women.
+          </blockquote>
         </p>
         <p>
-          Berkegiatan bersama kelompok anak muda ini, membuat energiku rasanya
-          seperti diisi ulang, dengan bersama-sama menumpahkan ide untuk
-          menggali berbagai cara agar dapat bergerak dengan keunikan yang harus
-          memiliki dampak nyata. Berbagai pandangan baru untuk mencegah dan
-          melawan kekerasan di lingkungan sendiri. Selama kelompok anak muda
-          berdaya, dengan jalannya sendiri, semakin pula menimbulkan banyak
-          pertanyaan dan pernyataan hingga tekanan dari berbagai pihak yang
-          menjadikan kami harus terus punya pondasi jati diri untuk tetap
-          bertahan dengan komitmen yang terus dibentuk dan diasah setiap
-          generasinya. Semakin lama bertumbuh beriringan dengan kelompok anak
-          muda ini, membuat diriku semakin sadar bahwa kalau kami ini membawa
-          perubahan yang nyata. Tidak usah terlalu jauh mengukur dan memandang
-          banyaknya masyarakat yang menerima manfaat, namun kami lah orang-orang
-          di dalamnya yang telah berhasil menjadi lebih baik dengan bertumbuh
-          bersama.
+          Pertemuan ini membahas tentang perempuan pekerja migran yang rentan
+          mengalami kekerasan seperti TPPO yang mirip dengan topik penelitian
+          skripsiku. Aku berpikir program ini tentu akan membantu proses
+          penulisan skripsiku, karena topiknya sangat sesuai dengan penelitian
+          yang sedang aku lakukan. Mengetahui hal ini, aku mengajukan diri untuk
+          berpartisipasi dalam setiap kegiatan program Safe and Fair. Aku hadir
+          sebagai representasi dari Mulead Lampung - sebuah komunitas anak muda
+          yang concern pada isu gender dan konselor sebaya bagi perempuan korban
+          kekerasan.
         </p>
         <p>
-          Kisahku sebagai penyintas kekerasan seksual dan perjalananku mencari
-          teman-teman yang searah untuk berjuang bersama melawan kekerasan,
-          ternyata menarik perhatian tim media Amerika untuk menjadikan kisahku
-          dan peran komunitas anak muda ini dalam sebuah film dokumenter yang
-          tayang di saluran VOA Indonesia yang berjudul “Healing is a Right”.
-          Dari sini kami sadari dan kembali kami refleksi bahwa kami tidak akan
-          ada dan berdampak sampai saat ini jika tidak ada “ibu” kami Yayasan
-          Embun Pelangi yang selalu memberikan penguatan pemahaman kepada kami,
-          salah satunya melalui program Safe and Fair untuk kami dapat terus
-          berdaya dan kuat bersuara.
+          Menurutku isu pekerja migran bukanlah isu yang menarik di kalangan
+          anak muda. Bukan isu “seksi” yang menarik untuk dijadikan topik
+          diskusi. Mungkin karena isu ini tidak relevan atau berdampak langsung
+          bagi anak-anak muda di sekitarku. Tapi aku melihat isu pekerja migran
+          khususnya bagi perempuan dan anak perempuan merupakan isu krusial yang
+          berdampak signifikan di tingkat global. Melalui kemampuan sederhanaku
+          memproduksi media kampanye, aku ikut berpartisipasi dalam penyusunan
+          RAD Provinsi Lampung untuk pencegahan dan penanganan TPPO, membentuk
+          kebijakan berupa SOP mekanisme penanganan kasus kekerasan yang dialami
+          pekerja migran oleh lembaga layanan, pertemuan lintas sektor dan
+          anggota gugus tugas, penguatan kapasitas bagi purna PMI, membantu
+          kepulangan perempuan-perempuan pekerja migran yang menjadi korban
+          TPPO, hingga kampanye kreatif dan aksi kolektif untuk meningkatkan
+          kesadaran publik akan isu TPPO yang rentan dialami oleh perempuan
+          pekerja migran.
+        </p>
+        <p>
+          Sebagai mahasiswa yang baru mengenal isu gender, terlibat dalam
+          program Safe and Fair ini membuka pemahamanku bahwa isu gender adalah
+          isu yang sangat kompleks.
+        </p>
+        <p>
+          Bagaimana perjuangan-perjuangan menuju kesetaraan ternyata juga masuk
+          dalam konteks migrasi internasional khususnya bagi mereka yang
+          digelari pahlawan devisa negara, pekerja migran. Seluruh proses itu
+          aku tuangkan ke dalam tulisan yang menjadi karya tulis ilmiah atau
+          skripsiku. Selama 1 tahun, diriku belajar seputar isu-isu krusial
+          khususnya memahami isu TPPO bagi pekerja migran sekaligus berproses
+          menjadi orang dewasa. Sebelumnya aku melihat skripsi adalah proses
+          yang sangat menyulitkan, aku merasa tidak yakin apakah aku mampu
+          melewatinya. Aku melihat teman-temanku berkutat pada buku-buku di
+          perpustakaan dan mengurung diri di kamar dari pagi ke pagi. Proses
+          yang membosankan dan membayangkannya saja aku sudah pusing. Namun
+          menulis skripsi berdasarkan pelaksanaan program Safe and Fair membuat
+          perjalananku menjadi lebih berwarna, senang bisa menuliskan
+          temuan-temuan di lapangan dan menjadikannya sebagai data ilmiah yang
+          valid.
+        </p>
+        <p>
+          Berakhirnya program Safe and Fair dan terselesaikannya skripsiku
+          bukanlah akhir dari jalanku terlibat dalam dunia pergerakan,
+          perjalananku 1 tahun kemarin adalah proses mengumpulkan amunisi
+          sebagai pejuang kesetaraan. Aku juga mengintrospeksi diri yang masih
+          memiliki banyak kekurangan ini, bagaimana belajar mengendalikan
+          keputusasaanku hidup di negeri yang belum menghargai perempuan. Besar
+          harapanku kedepannya untuk semakin dalam memahami isu gender sebagai
+          isu krusial, melalui pergumulanku di komunitas akar rumput. Pengalaman
+          itu tentu harus aku tuliskan agar menjadi sebuah pengetahuan yang tak
+          hanya aku miliki, tapi bisa aku bagikan kepada dunia. Seperti quotes
+          yang sering aku dengar di komunitas, bahwa pengalaman perempuan adalah
+          sumber pengetahuan.
         </p>
       </div>
     ),
