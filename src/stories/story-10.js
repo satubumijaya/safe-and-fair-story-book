de738d01694b750a694baeee72813259db35390c
kbprabowo <kuroceper2@gmail.com>
2024-02-06 02:05:10 +0700

    update stories story 10

--- src/stories/story-10.js
+++ src/stories/story-10.js
@@ -20,14 +20,18 @@ export const story10 = {
       <div>
         <p>
           <blockquote>
-            &ldquo;Anak muda zaman sekarang ya agak beda dari orang zaman dulu,
-            kalo sekarang kerja keras kurang, suka kritik, dikit-dikit kesehatan
-            mental, susah lah pokoknya banyak gak maunya.&rdquo;
+            &ldquo;
+            <em>
+              Anak muda zaman sekarang ya agak beda dari orang zaman dulu, kalo
+              sekarang kerja keras kurang, suka kritik, dikit-dikit kesehatan
+              mental, susah lah pokoknya banyak gak maunya
+            </em>
+            .&rdquo;
           </blockquote>
         </p>
         <p>
           Ucap salah satu peserta diskusi terfokus dalam kegiatan yang pernah
-          aku ikuti di tahun 2022.Ucapan ini langsung terngiang di kepalaku
+          aku ikuti di tahun 2022. Ucapan ini langsung terngiang di kepalaku
           hingga beberapa hari setelahnya, membuatku merasa tersindir sebagai
           generasi muda zaman ini. Ingin sekali kubuktikan bahwa ucapan peserta
           tersebut tidak sepenuhnya benar. Stigma tersebut tidak bisa
@@ -35,40 +39,41 @@ export const story10 = {
           28 tahun ini terbentur pada kapasitas dan pengalaman.
         </p>
         <p>
-          Program Safe and Fair dari UN Women yang telah bersamaku dan Yayasan
-          SAPA selama dua tahun ke belakang, membuatku belajar banyak terkait
-          advokasi. Advokasi ialah suatu proses untuk mempengaruhi orang lain
-          dan melalui proses advokasi inilah aku dan orang muda lainnya dapat
-          menyuarakan suara kami. Salah satu proses advokasi yang kami lakukan
-          ialah penyusunan revisi Rancangan Peraturan Gubernur Jawa Barat.
+          Program <em>Safe and Fair</em> dari UN Women yang telah bersamaku dan
+          Yayasan SAPA selama dua tahun ke belakang, membuatku belajar banyak
+          terkait advokasi. Advokasi ialah suatu proses untuk mempengaruhi orang
+          lain dan melalui proses advokasi inilah aku dan orang muda lainnya
+          dapat menyuarakan suara kami. Salah satu proses advokasi yang kami
+          lakukan ialah penyusunan revisi Rancangan Peraturan Gubernur Jawa
+          Barat yang terkait dengan perlindungan dan layanan untuk perempuan.
           Dimulai dengan pertemuan bersama para pemangku kepentingan atau
-          stakeholder terkait mendesaknya revisi kebijakan, pertemuan konsultasi
-          dan dialog bersama SKPD , hingga penyusunan daftar inventaris masalah
-          dari revisi Rapergub ini, aku dan orang muda lainya lalui dengan
-          berbagai rasa atau mixed feelings. Di satu sisi ada rasa bangga bahwa
-          aku dan orang muda dapat mengikuti forum-forum strategi ini dan
-          menjadi momen berharga kami untuk mengeluarkan pendapat pada para
-          pembuat kebijakan. Tapi di sisi lain, ada rasa takut salah dan minder
-          karena aku dan orang muda lainya tidak memiliki jam terbang dalam hal
-          advokasi, apalagi dalam menuliskan revisi kebijakan. Karena terlalu
-          sadar akan hal ini, aku selalu gelisah sebelum memulai kegiatan
-          advokasi.
+          stakeholder terkait mendesaknya&nbsp; revisi kebijakan, pertemuan
+          konsultasi dan dialog bersama Satuan Kerja Perangkat Daerah (SKPD),
+          hingga penyusunan daftar inventaris masalah untuk&nbsp; revisi
+          Rapergub ini, aku dan orang muda lainnya lalui dengan berbagai rasa.
+          Di satu sisi ada rasa bangga bahwa aku dan orang muda dapat mengikuti
+          forum-forum strategi ini dan menjadi momen berharga kami untuk
+          mengeluarkan pendapat pada para pembuat kebijakan. Tapi di sisi lain,
+          ada rasa takut salah dan minder karena aku dan orang muda lainya tidak
+          memiliki jam terbang dalam hal advokasi, apalagi dalam menuliskan
+          revisi kebijakan. Karena terlalu sadar akan hal ini, aku selalu
+          gelisah sebelum memulai kegiatan advokasi.
         </p>
         <p>
           Yang paling menegangkan adalah ketika aku dan orang muda lainya
-          berdialog dengan para stakeholder terkait tentang hasil rumusan kami
-          terhadap Rancangan Peraturan Gubernur Jawa Barat di tahun 2022. Di
-          hari itu entah kenapa terasa lebih panas dari biasanya sehingga
-          membuatku bercucuran keringat. Sesampainya di ruangan rapat yang telah
-          disediakan, kursi dan meja diatur berbentuk U yang membuat tiap orang
-          saling berhadapan. Yang hadir pada hari itu bukan hanya aku dan orang
-          muda, tetapi juga para aktivis senior yang sudah memiliki jam terbang
-          advokasi yang tinggi sehingga tidak terlihat raut gelisah di wajah
-          para senior ini. Sementara wajahku pasti sangat berbeda saat itu.
-          Kegelisahanku bertambah ketika aku ditunjuk sebagai fasilitator rapat
-          tersebut. Entah apa yang ada di pikiranku, aku mulai saja rapat itu
-          dengan salam dan tujuan kegiatan. Rapat hari itu berjalan lancar dan
-          tidak semenakutkan yang kubayangkan yaitu orang muda tidak mampu
+          berdialog dengan para pemangku kepentingan terkait tentang hasil
+          rumusan kami terhadap Rancangan Peraturan Gubernur Jawa Barat di tahun
+          2022. Di hari itu entah kenapa terasa lebih panas dari biasanya
+          sehingga membuatku bercucuran keringat. Sesampainya di ruangan rapat
+          yang telah disediakan, kursi dan meja diatur berbentuk U yang membuat
+          tiap orang saling berhadapan. Yang hadir pada hari itu bukan hanya aku
+          dan orang muda, tetapi juga para aktivis senior yang sudah memiliki
+          jam terbang advokasi yang tinggi sehingga tidak terlihat raut gelisah
+          di wajah para senior ini. Sementara wajahku pasti sangat berbeda saat
+          itu. Kegelisahanku bertambah ketika aku ditunjuk sebagai fasilitator
+          rapat tersebut. Entah apa yang ada di pikiranku, aku mulai saja rapat
+          itu dengan salam dan tujuan kegiatan. Rapat hari itu berjalan lancar
+          dan tidak semenakutkan yang kubayangkan yaitu orang muda tidak mampu
           memberikan suara dan pendapatnya sehingga kami ada hanya untuk
           memenuhi kuota pertemuan.
         </p>
@@ -79,9 +84,9 @@ export const story10 = {
           Jawa Barat. Diriku pun turut bangga bahwa salah satu penyusun dokumen
           usulan revisi Rancangan Peraturan Gubernur Jawa Barat yang juga
           diserahkan pada hari itu ialah aku. Ini adalah momen yang berharga,
-          aku dan orang muda lainnya bisa membuktikan kontribusi kami terhadap
-          pencegahan kekerasan berbasis gender dan perdagangan orang di Jawa
-          Barat.
+          aku dan orang muda lainnya bisa membuktikan&nbsp; kontribusi kami
+          terhadap pencegahan kekerasan berbasis gender dan perdagangan orang di
+          Jawa Barat.
         </p>
       </div>
     ),
@@ -89,14 +94,18 @@ export const story10 = {
       <div>
         <p>
           <blockquote>
-            &ldquo;Anak muda zaman sekarang ya agak beda dari orang zaman dulu,
-            kalo sekarang kerja keras kurang, suka kritik, dikit-dikit kesehatan
-            mental, susah lah pokoknya banyak gak maunya.&rdquo;
+            &ldquo;
+            <em>
+              Anak muda zaman sekarang ya agak beda dari orang zaman dulu, kalo
+              sekarang kerja keras kurang, suka kritik, dikit-dikit kesehatan
+              mental, susah lah pokoknya banyak gak maunya
+            </em>
+            .&rdquo;
           </blockquote>
         </p>
         <p>
           Ucap salah satu peserta diskusi terfokus dalam kegiatan yang pernah
-          aku ikuti di tahun 2022.Ucapan ini langsung terngiang di kepalaku
+          aku ikuti di tahun 2022. Ucapan ini langsung terngiang di kepalaku
           hingga beberapa hari setelahnya, membuatku merasa tersindir sebagai
           generasi muda zaman ini. Ingin sekali kubuktikan bahwa ucapan peserta
           tersebut tidak sepenuhnya benar. Stigma tersebut tidak bisa
@@ -104,40 +113,41 @@ export const story10 = {
           28 tahun ini terbentur pada kapasitas dan pengalaman.
         </p>
         <p>
-          Program Safe and Fair dari UN Women yang telah bersamaku dan Yayasan
-          SAPA selama dua tahun ke belakang, membuatku belajar banyak terkait
-          advokasi. Advokasi ialah suatu proses untuk mempengaruhi orang lain
-          dan melalui proses advokasi inilah aku dan orang muda lainnya dapat
-          menyuarakan suara kami. Salah satu proses advokasi yang kami lakukan
-          ialah penyusunan revisi Rancangan Peraturan Gubernur Jawa Barat.
+          Program <em>Safe and Fair</em> dari UN Women yang telah bersamaku dan
+          Yayasan SAPA selama dua tahun ke belakang, membuatku belajar banyak
+          terkait advokasi. Advokasi ialah suatu proses untuk mempengaruhi orang
+          lain dan melalui proses advokasi inilah aku dan orang muda lainnya
+          dapat menyuarakan suara kami. Salah satu proses advokasi yang kami
+          lakukan ialah penyusunan revisi Rancangan Peraturan Gubernur Jawa
+          Barat yang terkait dengan perlindungan dan layanan untuk perempuan.
           Dimulai dengan pertemuan bersama para pemangku kepentingan atau
-          stakeholder terkait mendesaknya revisi kebijakan, pertemuan konsultasi
-          dan dialog bersama SKPD , hingga penyusunan daftar inventaris masalah
-          dari revisi Rapergub ini, aku dan orang muda lainya lalui dengan
-          berbagai rasa atau mixed feelings. Di satu sisi ada rasa bangga bahwa
-          aku dan orang muda dapat mengikuti forum-forum strategi ini dan
-          menjadi momen berharga kami untuk mengeluarkan pendapat pada para
-          pembuat kebijakan. Tapi di sisi lain, ada rasa takut salah dan minder
-          karena aku dan orang muda lainya tidak memiliki jam terbang dalam hal
-          advokasi, apalagi dalam menuliskan revisi kebijakan. Karena terlalu
-          sadar akan hal ini, aku selalu gelisah sebelum memulai kegiatan
-          advokasi.
+          stakeholder terkait mendesaknya&nbsp; revisi kebijakan, pertemuan
+          konsultasi dan dialog bersama Satuan Kerja Perangkat Daerah (SKPD),
+          hingga penyusunan daftar inventaris masalah untuk&nbsp; revisi
+          Rapergub ini, aku dan orang muda lainnya lalui dengan berbagai rasa.
+          Di satu sisi ada rasa bangga bahwa aku dan orang muda dapat mengikuti
+          forum-forum strategi ini dan menjadi momen berharga kami untuk
+          mengeluarkan pendapat pada para pembuat kebijakan. Tapi di sisi lain,
+          ada rasa takut salah dan minder karena aku dan orang muda lainya tidak
+          memiliki jam terbang dalam hal advokasi, apalagi dalam menuliskan
+          revisi kebijakan. Karena terlalu sadar akan hal ini, aku selalu
+          gelisah sebelum memulai kegiatan advokasi.
         </p>
         <p>
           Yang paling menegangkan adalah ketika aku dan orang muda lainya
-          berdialog dengan para stakeholder terkait tentang hasil rumusan kami
-          terhadap Rancangan Peraturan Gubernur Jawa Barat di tahun 2022. Di
-          hari itu entah kenapa terasa lebih panas dari biasanya sehingga
-          membuatku bercucuran keringat. Sesampainya di ruangan rapat yang telah
-          disediakan, kursi dan meja diatur berbentuk U yang membuat tiap orang
-          saling berhadapan. Yang hadir pada hari itu bukan hanya aku dan orang
-          muda, tetapi juga para aktivis senior yang sudah memiliki jam terbang
-          advokasi yang tinggi sehingga tidak terlihat raut gelisah di wajah
-          para senior ini. Sementara wajahku pasti sangat berbeda saat itu.
-          Kegelisahanku bertambah ketika aku ditunjuk sebagai fasilitator rapat
-          tersebut. Entah apa yang ada di pikiranku, aku mulai saja rapat itu
-          dengan salam dan tujuan kegiatan. Rapat hari itu berjalan lancar dan
-          tidak semenakutkan yang kubayangkan yaitu orang muda tidak mampu
+          berdialog dengan para pemangku kepentingan terkait tentang hasil
+          rumusan kami terhadap Rancangan Peraturan Gubernur Jawa Barat di tahun
+          2022. Di hari itu entah kenapa terasa lebih panas dari biasanya
+          sehingga membuatku bercucuran keringat. Sesampainya di ruangan rapat
+          yang telah disediakan, kursi dan meja diatur berbentuk U yang membuat
+          tiap orang saling berhadapan. Yang hadir pada hari itu bukan hanya aku
+          dan orang muda, tetapi juga para aktivis senior yang sudah memiliki
+          jam terbang advokasi yang tinggi sehingga tidak terlihat raut gelisah
+          di wajah para senior ini. Sementara wajahku pasti sangat berbeda saat
+          itu. Kegelisahanku bertambah ketika aku ditunjuk sebagai fasilitator
+          rapat tersebut. Entah apa yang ada di pikiranku, aku mulai saja rapat
+          itu dengan salam dan tujuan kegiatan. Rapat hari itu berjalan lancar
+          dan tidak semenakutkan yang kubayangkan yaitu orang muda tidak mampu
           memberikan suara dan pendapatnya sehingga kami ada hanya untuk
           memenuhi kuota pertemuan.
         </p>
@@ -148,9 +158,9 @@ export const story10 = {
           Jawa Barat. Diriku pun turut bangga bahwa salah satu penyusun dokumen
           usulan revisi Rancangan Peraturan Gubernur Jawa Barat yang juga
           diserahkan pada hari itu ialah aku. Ini adalah momen yang berharga,
-          aku dan orang muda lainnya bisa membuktikan kontribusi kami terhadap
-          pencegahan kekerasan berbasis gender dan perdagangan orang di Jawa
-          Barat.
+          aku dan orang muda lainnya bisa membuktikan&nbsp; kontribusi kami
+          terhadap pencegahan kekerasan berbasis gender dan perdagangan orang di
+          Jawa Barat.
         </p>
       </div>
     ),
