dd59001648b423e5aa92425a31fa96079ab57be0
kbprabowo <kuroceper2@gmail.com>
2024-02-06 02:05:35 +0700

    update stories story 13

--- src/stories/story-13.js
+++ src/stories/story-13.js
@@ -22,20 +22,21 @@ export const story13 = {
           Saya Venolia Adeningrum, pendamping korban kekerasan berbasis gender.
           Banyak tantangan yang saya hadapi, namun bersama WCC Mawar Balqis saya
           tumbuh dan berkembang. Saya mulai bekerja sebagai pendamping korban
-          saat program Safe and Fair dilaksanakan oleh WCC Mawar Balqis. Sebagai
-          pendamping korban, banyak suka dan duka yang saya alami. Saya harus
-          mempunyai empati yang tinggi. Kasus yang ditangani adalah kasus
-          kekerasan seksual, kasus kekerasan dalam rumah tangga, kasus tindak
-          perdagangan orang, dan Pekerja Migran Indonesia.
+          saat program <em>Safe and Fair</em> dilaksanakan oleh WCC Mawar
+          Balqis. Sebagai pendamping korban, banyak suka dan duka yang saya
+          alami. Saya harus mempunyai empati yang tinggi. Kasus yang ditangani
+          adalah kasus kekerasan seksual, kasus kekerasan dalam rumah tangga,
+          kasus tindak perdagangan orang, dan Pekerja Migran Indonesia.
         </p>
         <p>
           Tantangan yang sering dihadapi yaitu kala korban ingin buru-buru
           segera menyelesaikan kasusnya dengan mengabaikan tahapan dan kode etik
           penyelesaian kasus. Ada juga korban tidak mematuhi peraturan yang
-          disepakati bersama. Tantangan lain adalah ketika posisi korban berada
-          di luar negeri atau lokasi rumah korban yang sulit diakses. Sebagai
-          pendamping saya juga harus belajar memahami karakteristik korban dan
-          bisa menjadi pendengar dan teman bicara yang baik bagi mereka.
+          disepakati bersama. Tantangan lain adalah ketika, posisi korban&nbsp;
+          berada di luar negeri atau lokasi rumah korban yang sulit diakses.
+          Sebagai pendamping saya juga harus belajar memahami karakteristik
+          korban dan bisa menjadi pendengar dan teman bicara yang baik bagi
+          mereka.
         </p>
         <p>
           Selama menjadi pendamping korban, banyak cerita menyesakkan hati,
@@ -47,26 +48,27 @@ export const story13 = {
           ternyata suaminya tersebut sudah tega memperkosa anaknya sendiri.
           Setelah kembali dari kerja di luar negeri, saya dan tim di WCC Mawar
           Balqis mendampingi korban untuk menghadapi dan menceritakan kejadian
-          tersebut. Awalnya ibu korban syok, “Hati ibu mana yang tak sakit hati
-          melihat anaknya sendiri disakiti oleh seseorang yang bertahun-tahun
-          saya cintai,” tutur ibu korban sebut saja Martiah. “Terima kasih mbak
-          pendamping sudah mendampingi proses hukum korban, saya senang bisa
-          bertemu dengan WCC Mawar Balqis.” Rasanya lelah terbayar tuntas ketika
-          korban menghargai pendampingan yang saya lakukan meski hanya dengan
-          kata terima kasih. Meskipun masih banyak yang memandang kasus ini
-          sebelah mata dan bahkan menyalahkan korban, seperti yang dilakukan
-          oleh keluarga pelaku. Keinginan keluarga pelaku adalah mendapatkan
-          mediasi agar bisa menempuh jalur kekeluargaan dan jalur damai.
+          tersebut. Awalnya ibu korban syok, &ldquo;Hati ibu mana yang tak sakit
+          hati melihat anaknya sendiri disakiti oleh seseorang yang
+          bertahun-tahun saya cintai,&rdquo; tutur ibu korban sebut saja
+          Martiah. &ldquo;Terima kasih mbak pendamping sudah mendampingi proses
+          hukum korban, saya senang bisa bertemu dengan WCC Mawar Balqis.&rdquo;
+          Rasanya lelah terbayar tuntas ketika korban menghargai pendampingan
+          yang saya lakukan meski hanya dengan kata terima kasih. Meskipun&nbsp;
+          masih banyak yang memandang kasus ini sebelah mata dan bahkan
+          menyalahkan korban, seperti yang dilakukan oleh keluarga pelaku.
+          Keinginan keluarga pelaku adalah mendapatkan mediasi agar bisa
+          menempuh jalur kekeluargaan dan jalur damai.
         </p>
         <p>
           Pengalaman menantang lainnya adalah ketika terjadi kasus pemerkosaan
           inses antara bapak dan anak kandung hingga korban hamil di desa.
           Sebagai pendamping saya makin yakin bahwa penyelesaian adat tidak
-          berpihak kepada korban. WCC Mawar Balqis menyediakan layanan shelter
-          sambil menunggu kelahiran anak korban. Ini adalah pengalaman saya yang
-          kesekian kali mendampingi korban saat melahirkan. Seperti merasakan
-          sakitnya! Memang benar bahwa korban berani bersuara itu hebat! serius
-          nggak pake becanda!
+          berpihak kepada korban. WCC Mawar Balqis menyediakan layanan&nbsp;{" "}
+          <em>shelter</em> sambil menunggu kelahiran anak korban. Ini adalah
+          pengalaman saya yang kesekian kali mendampingi korban saat melahirkan.
+          Seperti merasakan sakitnya!. Memang benar bahwa korban berani bersuara
+          itu hebat! serius nggak pake becanda !
         </p>
         <p>
           <blockquote>
@@ -104,20 +106,21 @@ export const story13 = {
           Saya Venolia Adeningrum, pendamping korban kekerasan berbasis gender.
           Banyak tantangan yang saya hadapi, namun bersama WCC Mawar Balqis saya
           tumbuh dan berkembang. Saya mulai bekerja sebagai pendamping korban
-          saat program Safe and Fair dilaksanakan oleh WCC Mawar Balqis. Sebagai
-          pendamping korban, banyak suka dan duka yang saya alami. Saya harus
-          mempunyai empati yang tinggi. Kasus yang ditangani adalah kasus
-          kekerasan seksual, kasus kekerasan dalam rumah tangga, kasus tindak
-          perdagangan orang, dan Pekerja Migran Indonesia.
+          saat program <em>Safe and Fair</em> dilaksanakan oleh WCC Mawar
+          Balqis. Sebagai pendamping korban, banyak suka dan duka yang saya
+          alami. Saya harus mempunyai empati yang tinggi. Kasus yang ditangani
+          adalah kasus kekerasan seksual, kasus kekerasan dalam rumah tangga,
+          kasus tindak perdagangan orang, dan Pekerja Migran Indonesia.
         </p>
         <p>
           Tantangan yang sering dihadapi yaitu kala korban ingin buru-buru
           segera menyelesaikan kasusnya dengan mengabaikan tahapan dan kode etik
           penyelesaian kasus. Ada juga korban tidak mematuhi peraturan yang
-          disepakati bersama. Tantangan lain adalah ketika posisi korban berada
-          di luar negeri atau lokasi rumah korban yang sulit diakses. Sebagai
-          pendamping saya juga harus belajar memahami karakteristik korban dan
-          bisa menjadi pendengar dan teman bicara yang baik bagi mereka.
+          disepakati bersama. Tantangan lain adalah ketika, posisi korban&nbsp;
+          berada di luar negeri atau lokasi rumah korban yang sulit diakses.
+          Sebagai pendamping saya juga harus belajar memahami karakteristik
+          korban dan bisa menjadi pendengar dan teman bicara yang baik bagi
+          mereka.
         </p>
         <p>
           Selama menjadi pendamping korban, banyak cerita menyesakkan hati,
@@ -129,26 +132,27 @@ export const story13 = {
           ternyata suaminya tersebut sudah tega memperkosa anaknya sendiri.
           Setelah kembali dari kerja di luar negeri, saya dan tim di WCC Mawar
           Balqis mendampingi korban untuk menghadapi dan menceritakan kejadian
-          tersebut. Awalnya ibu korban syok, “Hati ibu mana yang tak sakit hati
-          melihat anaknya sendiri disakiti oleh seseorang yang bertahun-tahun
-          saya cintai,” tutur ibu korban sebut saja Martiah. “Terima kasih mbak
-          pendamping sudah mendampingi proses hukum korban, saya senang bisa
-          bertemu dengan WCC Mawar Balqis.” Rasanya lelah terbayar tuntas ketika
-          korban menghargai pendampingan yang saya lakukan meski hanya dengan
-          kata terima kasih. Meskipun masih banyak yang memandang kasus ini
-          sebelah mata dan bahkan menyalahkan korban, seperti yang dilakukan
-          oleh keluarga pelaku. Keinginan keluarga pelaku adalah mendapatkan
-          mediasi agar bisa menempuh jalur kekeluargaan dan jalur damai.
+          tersebut. Awalnya ibu korban syok, &ldquo;Hati ibu mana yang tak sakit
+          hati melihat anaknya sendiri disakiti oleh seseorang yang
+          bertahun-tahun saya cintai,&rdquo; tutur ibu korban sebut saja
+          Martiah. &ldquo;Terima kasih mbak pendamping sudah mendampingi proses
+          hukum korban, saya senang bisa bertemu dengan WCC Mawar Balqis.&rdquo;
+          Rasanya lelah terbayar tuntas ketika korban menghargai pendampingan
+          yang saya lakukan meski hanya dengan kata terima kasih. Meskipun&nbsp;
+          masih banyak yang memandang kasus ini sebelah mata dan bahkan
+          menyalahkan korban, seperti yang dilakukan oleh keluarga pelaku.
+          Keinginan keluarga pelaku adalah mendapatkan mediasi agar bisa
+          menempuh jalur kekeluargaan dan jalur damai.
         </p>
         <p>
           Pengalaman menantang lainnya adalah ketika terjadi kasus pemerkosaan
           inses antara bapak dan anak kandung hingga korban hamil di desa.
           Sebagai pendamping saya makin yakin bahwa penyelesaian adat tidak
-          berpihak kepada korban. WCC Mawar Balqis menyediakan layanan shelter
-          sambil menunggu kelahiran anak korban. Ini adalah pengalaman saya yang
-          kesekian kali mendampingi korban saat melahirkan. Seperti merasakan
-          sakitnya! Memang benar bahwa korban berani bersuara itu hebat! serius
-          nggak pake becanda!
+          berpihak kepada korban. WCC Mawar Balqis menyediakan layanan&nbsp;{" "}
+          <em>shelter</em> sambil menunggu kelahiran anak korban. Ini adalah
+          pengalaman saya yang kesekian kali mendampingi korban saat melahirkan.
+          Seperti merasakan sakitnya!. Memang benar bahwa korban berani bersuara
+          itu hebat! serius nggak pake becanda !
         </p>
         <p>
           <blockquote>
