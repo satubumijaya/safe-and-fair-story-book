b8a038bd054b8e9b73cfa3594782790e470fbff1
Kusuma Hendra <k.kusumahendra@gmail.com>
2024-04-02 23:36:34 +0800

    feat: story 11-15 english

--- src/stories/story-12.js
+++ src/stories/story-12.js
@@ -12,40 +12,32 @@ export const story12 = {
   },
   name: "Faoziah",
   title: {
-    en: "Perubahan ini Kupersembahkan untuk Ibuku",
+    en: "I Dedicate This Change to My Mother",
     id: "Perubahan ini Kupersembahkan untuk Ibuku",
   },
   content: {
     en: (
       <div>
+        <p>My name is Faoziah, I am 40 years old. I come from Kedongdong Village, Susukan Subdistrict, Cirebon Regency. I am an only child, so naturally I am the only hope for my mother, who is a single parent. I was born to a great woman, who, in order to make her child happy and support her large family, was willing to become a &ldquo;foreign exchange hero&rdquo; in some faraway Middle Eastern country.</p>
+        <p>When I was three years old, my mother decided to become a migrant worker, and returned to Indonesia only once every two or three years. That was the case until I was 21 years old. From her hard work, everything that mothers dreamed of and whatever I wanted could be fulfilled. From building a house, owning a rice field and buying a plot of land, to buying a vehicle, opening a clothing business and sending me to school at the Academy of Hospital Administration Management at AMA Yogyakarta up to the Diploma 3 level.</p>
         <p>
-          Namaku Faoziah, umurku 40 tahun. Aku berasal dari Desa Kedongdong, Kec. Susukan, Kab. Cirebon. Aku merupakan anak tunggal yang tentunya menjadi harapan satu-satunya bagi ibuku yang seorang
-          <em>single parent.</em> Aku terlahir dari wanita hebat, yang demi membahagiakan anak serta menghidupi keluarga besarnya, beliau rela menjadi seorang &ldquo;pahlawan devisa&rdquo; di negeri Timur Tengah sana.
+          When I was in the fourth semester of college, I decided to get married. After graduating from college, I worked at a hospital, owned by the Cirebon Regency government, for one and a half years. However, there were several things to consider, one of which was my husband&rsquo;s request that I focus on taking care of our child, as the situation at that time did not allow me to leave my child to work. Our baby was very fussy, and had a health problem, so with a heavy heart I decided to resign from my job. Before doing so, I asked my mother&rsquo;s opinion, but she only answered, &ldquo;Do whatever you think is best<em>.</em>&rdquo; I know there was disappointment in her answer. My mother had done her best for me, but I was unable to repay her services by working according to the level of education I had completed. But at that time, I had no other choice.
         </p>
-        <p>Sedari aku berumur tiga tahun, ibuku memutuskan menjadi seorang PMI dan pulang ke Indonesia hanya dua atau tiga tahun sekali. Itu terjadi sampai aku berusia 21 tahun. Dari kerja kerasnya itu, semua yang ibu impikan serta apapun yang aku inginkan dapat terpenuhi. Dari mulai membangun rumah, memiliki sepetak sawah, membeli sebidang tanah, membeli kendaraan, membuka usaha pakaian serta menyekolahkanku hingga Diploma 3 Akademi Manajemen Administrasi Rumah Sakit di AMA Yogyakarta.</p>
+        <p>After six years of not working and choosing to focus on being a housewife, I feel a loss of interest in coming back to work. Sometimes, I felt hurt when people around me ridiculed and sneered at me, with comments like, &ldquo;It&rsquo;s a waste of money getting her to higher education, to the point that her mother was willing to become a migrant worker. But look, now she is unemployed.&rdquo; Every time people around me spoke words like that, I knew for sure my mother felt the same heartache as I did.&nbsp;&nbsp;</p>
+        <p>Thank you, Mom, for understanding and sacrificing a lot for me.</p>
+        <p>In 2021, when I became a village cadre, I received an assignment from the village government to attend an activity with the Women Crisis Center (WCC) Mawar Balqis. This was where I first met Ms. Dian, an inspirational figure from the center. At first, I wasn&rsquo;t confident and wasn't familiar with migrant workers' issues and their challenges. But because of the enthusiasm, motivation, and open-hearted attitude of my friends at WCC Mawar Balqis, I was finally interested in joining and being involved. Thank God, my husband allows me to participate and supports me.</p>
+        <p>From then on, I found the enthusiasm within myself again to make my life better and more useful for many people.&nbsp; There are many things I have participated in after joining WCC Mawar Balqis, including assisting victims, handling cases, providing counseling assistance, participating in training such as paralegal and gender equality training, service standards and integrated protection mechanisms, and many more positive activities and new areas for me.</p>
+        <p>There are many changes and lessons that I could learn from the decisions I made. The positive change I feel is that I am now more sensitive, more caring, and more responsive when there are cases happening around me. I have also become more enthusiastic and feel there are new challenges in my life. What is most noticeable is that the people who used to sneer at me now no longer look down on me. Even though I realize that this change is still not comparable with all my mother&rsquo;s sacrifices and struggles, at least I can make a change to become a better person than before and will continue the process.</p>
+        <p>I have experienced not just good changes. There have also been many challenges, such as obtaining my husband&rsquo;s permission to participate in lengthy activities or to travel outside our town, conflicts of interest with the village government, as well as poor responses from the community.&nbsp;</p>
         <p>
-          Ketika kuliah di semester empat, aku memutuskan untuk menikah. Setelah lulus kuliah, aku sempat bekerja di sebuah rumah sakit milik pemerintah Kabupaten Cirebon selama 1,5 tahun. Namun ada beberapa pertimbangan, salah satunya permintaan suami yang ingin aku untuk fokus mengurus anak kami yang kondisinya saat itu memang sangat tidak memungkinkan untuk ditinggal bekerja. Buah hati kami memang sangat rewel dan kesehatannya juga sangat ringkih, maka dengan berat hati aku memutuskan untuk <em>resign</em> dari pekerjaanku. Sebelumnya aku sempat meminta pendapat ibuku, namun beliau hanya menjawab, &ldquo;
-          <em>Lakukan apapun yang menurutmu baik</em>.&rdquo; Aku tahu pasti ada kekecewaan dari jawabannya. Ibuku sudah mengusahakan yang terbaik untukku, namun nyatanya aku belum mampu membalas jasa ibuku dengan bekerja sesuai dengan jenjang pendidikan yang aku tempuh. Namun saat itu aku tak punya pilihan lain.
+          <blockquote>We often hear sharp and offensive words thrown at us at WCC Mawar Balqis, but it is not a big problem nor an obstacle, because we do or work with love and sincerity.</blockquote>
         </p>
-        <p>Setelah 6 tahun tidak bekerja dan memilih fokus menjadi ibu rumah tangga, mimpiku untuk kembali bekerja bak hilang ditelan bumi. Terkadang ada rasa sakit hati kala orang-orang di sekitar mencemooh dan mencibirku seperti,</p>
+        <p>I will not stop here and will continue the process. Hopefully, I can continue to improve and become a useful person to many people.</p>
         <p>
-          <blockquote>
-            &ldquo;
-            <em>Percuma ya buang-buang uang untuk menyekolahkan dia setinggi mungkin sampai ibunya rela menjadi PMI, tapi sekarang malah jadi pengangguran</em>
-            .&rdquo; Setiap kata seperti itu terucap dari orang sekitarku, aku tahu dengan pasti ibuku merasakan sakit hati yang sama seperti yang aku rasakan.
-          </blockquote>
-        </p>
-        <p>Terimakasih, untuk Ibu sudah mau mengerti dan berkorban banyak untukku.</p>
-        <p>Di tahun 2021 saat aku menjadi kader desa, aku mendapatkan tugas dari pemerintah desa untuk menghadiri sebuah kegiatan bersama Women Crisis Center (WCC) Mawar Balqis. Di sinilah awal mula aku dipertemukan dengan Mbak Dian, sosok inspiratif dari WCC Mawar Balqis. Awalnya aku tidak percaya diri dan merasa asing dengan dunia PMI serta segala permasalahan yang ada di dalamnya. Tapi karena semangat, motivasi, dan sikap terbuka dari teman-teman di WCC Mawar Balqis, akhirnya aku pun tertarik untuk bergabung dan terlibat di dalamnya. Alhamdulillah suami pun mengizinkan serta mendukungku selagi aku bisa membagi waktu antara keluarga dan kegiatanku.</p>
-        <p>Dari situlah aku seperti menemukan lagi semangat dalam diri untuk menjadikan hidupku lebih baik lagi dan berguna bagi banyak orang. Banyak yang sudah aku ikuti setelah bergabung dengan WCC Mawar Balqis baik itu pendampingan korban, penanganan kasus, pendampingan konseling, pelatihan-pelatihan seperti Paralegal, kesetaraan gender, standar layanan dan mekanisme perlindungan terpadu, dan masih banyak lagi kegiatan-kegiatan positif dan hal baru untukku.</p>
-        <p>Banyak perubahan dan hikmah yang bisa aku petik dari keputusan yang aku ambil ini. Perubahan positif yang kurasa adalah kini aku menjadi lebih peka, lebih peduli, dan lebih cepat tanggap saat ada kasus yang terjadi di sekitarku. Aku juga menjadi lebih bersemangat dan merasa ada tantangan baru dalam hidupku. Dan yang paling terasa lagi yaitu, orang-orang yang dulu mencibirku kini sudah tidak lagi memandangku dengan sebelah mata. Meskipun aku sadar perubahan ini tetap tidak sepadan dengan semua pengorbanan dan perjuangan ibuku, tapi setidaknya aku bisa melakukan sebuah perubahan menjadi orang yang lebih baik dari sebelumnya dan akan terus berproses.</p>
-        <p>Tidak hanya perubahan baik yang aku dapat. Banyak juga tantangan mulai dari izin suami ketika akan berkegiatan yang memakan waktu lama atau berkegiatan ke luar kota, konflik kepentingan dengan pemerintah desa dan juga respon yang kurang baik dari masyarakat. Kata-kata pedas sudah menjadi makanan sehari-hari untuk kami (WCC Mawar Balqis), tapi itu bukanlah masalah besar dan menjadi penghalang, karena kami melakukannya dengan cinta dan ketulusan. Aku takkan berhenti di sini dan akan terus berproses. Semoga aku bisa lebih baik lagi dan bisa terus menjadi orang yang berguna bagi banyak orang.</p>
-        <p>
-          Untuk perempuan terhebat dalam hidupku, hanya satu kata yang bisa ku ucapkan, &ldquo;Perubahan ini aku persembahkan untukmu, ibu."
-          <em>I Love you Mom</em>.&rdquo;
+          To the most incredible woman in my life, there is only one thing I can say: &ldquo;I dedicate this change to you, Mother. <em>I love you, Mom</em>.&rdquo;
         </p>
         <p>
-          <em>Faoziah adalah Pendamping Komunitas WCC Mawar Balqis Cirebon</em>
+          <em>Faoziah is a Companion at WCC Mawar Balqis Cirebon Community.</em>
         </p>
       </div>
     ),
