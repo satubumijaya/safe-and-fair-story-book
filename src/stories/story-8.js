4ab2f0999ee3959eb03b2778a93a63aafd15f016
Kusuma Hendra <k.kusumahendra@gmail.com>
2024-04-02 23:18:16 +0800

    feat: story 6-10 english

--- src/stories/story-8.js
+++ src/stories/story-8.js
@@ -12,46 +12,34 @@ export const story8 = {
   },
   name: "Roudlotul Jannah",
   title: {
-    en: "Perubahan adalah Tanggung Jawab Bersama",
+    en: "Change is a Shared Responsibility",
     id: "Perubahan adalah Tanggung Jawab Bersama",
   },
   content: {
     en: (
       <div>
         <p>
-          <em>&ldquo;Wis mbak bli papa, kita ora melu ning komunitas tapi Insya Allah kita siap melu kegiatan. Baka ana apa-apa kari hubungi bae kita ya mbak&rdquo;</em>
-          (tidak apa-apa mbak, saya tidak ikut dalam komunitas juga insya Allah saya siap ikut dalam kegiatan. Dan kalau ada apa-apa tinggal hubungi saja saya ya)
-        </p>
-        <p>Kalimat yang menurut saya sangat menguatkan sekali yang keluar dari seorang ibu rumah tangga, seorang kader Posyandu Desa yang memiliki rasa kepedulian sangat tinggi untuk perempuan-perempuan pekerja migran sekitarnya. Walau keadaan di sana sedang hiruk pikuk setelah adanya pesta demokrasi di desa, tetapi hambatan itu tidak menyurutkan semangatnya sama sekali.</p>
-        <p>
-          Sengat teriknya matahari pada hari itu adalah kali pertama pertemuan kami dimulai. Kekhawatiran banyak orang dalam situasi COVID-19 membuat sedikit aktivitas yang berjalan. Program <em>Safe and Fair</em> adalah jembatan bagi saya dan teman-teman di Desa Kedongdong untuk bisa bertemu dan juga berbagi. Sebut saja Faoziah namanya, perkenalan awal saya dengannya sangat singkat. Sama-sama tidak kenal dan saya sedikit memberikan gambaran tujuan saya untuk bisa berbagi informasi terkait migrasi aman di Desa Kedongdong ini. Dengan semangat dan sedikit ragu dengan dirinya sendiri, dia menjawab &ldquo;baiklah saya akan membantu dan mencoba untuk ikut dalam kegiatan ini&rdquo;. Berbinar saya mendengar jawabannya dan terucap rasa syukur dalam hati.
+          &ldquo;<em>Wis mbak bli papa, kita ora melu ning komunitas tapi Insya Allah kita siap melu kegiatan. Baka ana apa-apa kari hubungi bae kita ya mbak</em>&rdquo; (It&rsquo;s okay, though I&rsquo;m not involved in the community, I&rsquo;m ready to take part in its activities. And if there&rsquo;s anything, just contact me.)
         </p>
         <p>
-          Seiring berjalannya waktu, saya terus berkomunikasi intens melalui
-          <em>Whatsapp</em> dengan Ibu Faoziah karena memang jarak rumah kami lumayan berjauhan dan hanya beberapa kali saja kami bertemu dalam satu bulan. Kami membuat rencana kegiatan bersama, membuat daftar warga desa perempuan yang akan dilibatkan dalam kegiatan dan juga sedikit membuat pemetaan untuk sosialisasi. Walaupun saat itu kami berkomunikasi hanya melalui handphone, tapi kami merasa <em>&ldquo;nyambung&rdquo;</em>.
+          That sentence, which I think was very encouraging, came from a housewife, a Village integrated health post (<em>Posyandu</em>) cadre who is strongly concerned about women migrant workers around her. Even though things were busy there after the election day in the village, these obstacles did not dampen her enthusiasm at all.
         </p>
-        <p>&ldquo;Mbak, saya kumpulin orang-orang sekitar saya saja dulu ya mbak untuk pertemuan awal kita,&rdquo; kata Ibu Faoziah</p>
-        <p>&ldquo;Iya bu tidak apa, sembari nanti kita bersilaturahmi ke ibu Kuwu yang baru, barangkali ada rekomendasi lainnya,&rdquo; sahut saya.</p>
-        <p>Pertemuan dan sosialisasi pertama kami berjalan lancar di rumah Ibu Faoziah, yang pada awalnya ibu-ibu ini masih malu-malu. Ternyata di akhir kegiatan, mereka banyak bercerita dan curhat terkait masih banyak perempuan-perempuan yang berangkat ke Timur Tengah secara non-prosedural di masa pandemi Covid-19. Ada juga yang kehilangan kontak dengan keluarganya dan juga banyak dari mereka yang menuakan usia untuk memenuhi syarat pemberangkatan mereka bekerja.</p>
-        <p>Seusai pertemuan, biasanya saya mengobrol bersama dengan Ibu Faoziah, terkait masih banyaknya warga di desa ini yang berangkat secara non-prosedural. Informasi yang diterima oleh masyarakat juga sangat kurang, apalagi saat itu kondisi pemerintah desa yang sepertinya kurang peduli. Tapi dengan adanya pemerintahan baru ini, ada harapan baru semoga pemerintah desa lebih peduli lagi dengan masyarakat khususnya dengan perempuan pekerja migran dan juga keluarganya.</p>
+        <p>It was already hot by the time our first meeting began. People&rsquo;s concerns about the COVID-19 situation had resulted in few activities taking place. The Safe and Fair programme was a bridge for me and my friends in Kedongdong Village to be able to meet and share.&nbsp;</p>
+        <p>Call her Faoziah. My initial introduction to her was very short. Neither of us had known each other, and I gave her a little idea of my aim to be able to share information regarding safe migration in Kedongdong Village. Enthusiastic despite her mild doubt, she answered, &ldquo;Okay, I will help and try to take part in this activity.&rdquo; I was beaming when I heard the answer, and expressed the gratitude in my heart.</p>
+        <p>As time passed, I continued to communicate intensely via WhatsApp with Ms. Faoziah because our homes were quite far apart and we only met a few times a month. We made a joint activity plan, made a list of women villagers who would be involved in the activities and also made a little mapping for dissemination activities. Even though at that time we only communicated via cellphone, we felt &ldquo;connected.&rdquo;</p>
+        <p>&ldquo;I&rsquo;ll just gather the people around me first, for our initial meeting,&rdquo; said Ms. Faoziah.</p>
+        <p>&ldquo;Yes, that&rsquo;s okay, while we&rsquo;re visiting Ibu Kuwu (Village Head), maybe there are other recommendations,&rdquo; I said.</p>
+        <p>Our first meeting and dissemination went smoothly at Ms. Faoziah&rsquo;s house, where the women were initially still shy.&nbsp; It turned out that at the end of the activity, they talked and shared how a lot of women still went to the Middle East non-procedurally during the COVID-19 pandemic. Some had lost contact with their families, and many of them were old enough to meet the requirements for leaving for work.&nbsp;</p>
+        <p>After the meetings, I usually chatted with Ms. Faoziah, regarding the large number of residents in this village who had left non-procedurally. The information received by the community was also lacking, especially at that time the village government seemed less concerned with the issue. But with this new government, there was new hope that the village government would care more about the community, especially women migrant workers and their families.</p>
+        <p>Indeed, on every trip, there will always be "surprises," just as Ms. Faoziah and I experienced right away. With the new village government, our hopes were dashed because Mrs. Kuwu, the village head's wife, asked Ms. Faoziah to be replaced as community coordinator by Mrs. Kuwu's cadre.&nbsp; I had very high hopes for the new government at that time, hoping that Ms. Faoziah, who had been working with the village community, would get support, but of course, every village has its own interesting story. As a result of this change, my enthusiasm dropped. I thought about what I had done with Ms. Faoziah before, over and over again. With the changed conditions, I had to do everything from zero again. However, Ms. Faoziah motivated me again by telling me that this was not the end of everything. Although Ms. Faoziah is no longer working in the community, her enthusiasm for contributing to the women in her village has remained strong. Because of that, we continue to involve Mrs. Faoziah.</p>
+        <p>Ms. Faoziah's enthusiasm also boosts my enthusiasm and determination to give my best, especially in disseminating information related to women migrant workers. We continue to involve Ms. Faoziah in various activities to increase her capacity, such as paralegal training, advocacy with local governments, and other activities to grow her confidence in speaking at forums and expanding her knowledge.&nbsp;</p>
+        <p>For me, Ms. Faoziah has been a friend, teacher, and role model. Despite various limitations, she is always enthusiastic and helpful. To this day, we continue to communicate and share information. For example, several cases occurred in the village last July, which Ms. Faoziah enthusiastically shared with us and "quickly" assisted the victims' families. With this information, we could also coordinate with related parties to respond.&nbsp;</p>
         <p>
-          Memang dalam setiap perjalanan itu akan selalu ada
-          <em>&ldquo;surprisenya&rdquo;,</em> seperti yang sedang saya dan Ibu Faoziah alami sekarang. Dengan adanya pemerintahan desa yang baru, harapan kami ternyata pupus, karena Ibu Kuwu, istri kepala desa, meminta Ibu Faoziah sebagai koordinator komunitas diganti dengan Kadernya.
+          <blockquote>There are many things that I have learned on this journey, including that every obstacle is not the end of everything. Small things that other people think are useless can become big things and can be useful for many people.&nbsp;</blockquote>
         </p>
+        <p>That is the effect of Ms. Faoziah&rsquo;s presence in her village.</p>
         <p>
-          <blockquote>Harapan saya sangat besar dengan pemerintahan baru saat itu, berharap bahwa Ibu Faoziah yang sudah bergerak bersama masyarakat desa tersebut, seharusnya mendapatkan dukungan, tapi ya sudahlah pasti setiap desa memiliki cerita menariknya sendiri.</blockquote>
-        </p>
-        <p>
-          Ternyata, semangat saya sempat turun karena adanya pergantian ini. Terlalu <em>overthinking </em>dengan apa yang sudah saya lakukan dengan Ibu Faoziah sebelumnya. Karena dengan kondisi sekarang, saya harus melakukan semuanya dari &ldquo;0&rdquo; lagi. Namun, lagi-lagi Ibu Faoziah menampar saya dengan memberikan semangat bahwa ini bukan akhir dari segalanya. Dengan tidak terikatnya Ibu Faoziah bersama komunitas, tidak menyurutkan semangatnya untuk bisa menjadi orang yang bermanfaat untuk perempuan-perempuan di desanya. Karena itu kami tetap melibatkan Ibu Faoziah secara mandiri.
-        </p>
-        <p>Dengan semangat Ibu Faoziah, ini juga menumbuhkan semangat dan tekad saya untuk bisa memberikan yang terbaik. Terutama informasi-informasi yang berkaitan dengan Perempuan Pekerja Migran. Berbagai kegiatan untuk peningkatan kapasitas, kami tetap melibatkan Ibu Faoziah di dalamnya. Terkait pelatihan paralegal, melakukan advokasi dengan pemerintahan daerah, dan berbagai kegiatan lainnya untuk bisa menumbuhkan kepercayaan diri Ibu Faoziah berbicara dalam forum dan juga untuk memperluas informasinya.</p>
-        <p>
-          Bagi saya Ibu Faoziah adalah, teman, guru dan panutan saya. Walaupun dengan berbagai keterbatasan dia tetap semangat untuk bisa menjadi orang yang bermanfaat dan bisa berbagi dengan orang lain. Sampai saat ini kami terus berkomunikasi dan berbagi informasi. Seperti pada Juli lalu ada beberapa kasus yang terjadi di desa tersebut, dengan semangatnya Ibu Faoziah berbagi dengan kami dan langsung
-          <em> &ldquo;gercep&rdquo;</em> melakukan pendampingan kepada keluarga korban. Dengan informasi tersebut kami juga bisa berkoordinasi dengan pihak-pihak terkait dalam penanganannya.
-        </p>
-        <p>Banyak hal yang bisa saya pelajari bersama dalam perjalanan ini, bahwa setiap kendala yang ada itu bukan akhir dari segalanya. Hal kecil yang menurut orang lain itu tidak ada gunanya bisa menjadi hal yang besar dan bisa bermanfaat untuk banyak orang. Itu adalah keberadaan Ibu Faoziah di desanya.</p>
-        <p>
-          <em>Dian Roudlotul Jannah adalah Pendamping Komunitas Desa dan Pesantren di WCC Mawar Balqis Cirebon</em>
+          <em>Roudlotul Jannah is a Village and Islamic Boarding School Community Facilitator at WCC Mawar Balqis Cirebon.</em>
         </p>
       </div>
     ),
