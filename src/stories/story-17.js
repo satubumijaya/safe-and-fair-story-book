b639a27268c14b23d183de09600e824ef9c9ead0
Kusuma Hendra <k.kusumahendra@gmail.com>
2024-04-02 23:49:01 +0800

    feat: story 16 - 19 english

--- src/stories/story-17.js
+++ src/stories/story-17.js
@@ -12,24 +12,22 @@ export const story17 = {
   },
   name: "Dira Septiani",
   title: {
-    en: "Langkah Awal Keluar dari Zona Nyaman “Membangun Kebiasaan Positif dan Membentuk Diri Menjadi Lebih Baik”",
+    en: "First Steps Out of the Comfort Zone: “Building Positive Habits and Shaping Yourself to Become Better”",
     id: "Langkah Awal Keluar dari Zona Nyaman “Membangun Kebiasaan Positif dan Membentuk Diri Menjadi Lebih Baik”",
   },
   content: {
     en: (
       <div>
-        <p>Dalam keheningan malam yang tenang, saya gulir foto-foto lama yang mengingatkan pada momen-momen penting dalam hidup. Saya ingat sekali pada masa itu pasca pandemi COVID-19, jiwa saya masih berharap menjadi anak SMA yang seutuhnya. Momen&ndash;momen berharga yang telah direnggut itu menjadikan saya malas dan mengurung diri untuk menjangkau dunia luar. Hal ini pun berlanjut ketika saya memasuki dunia perkuliahan tepatnya pada tahun 2021. Di saat semua orang mempunyai banyak teman dan relasi, saya hanya sendiri dengan bayang- bayang untuk menggapai cita &ndash; cita. Saya ingin menjadi orang yang bermanfaat untuk orang lain. Bisa berbagi ilmu pengetahuan merupakan perwujudan dari cita-cita saya menjadi seorang dosen atau tenaga pengajar. Saya berpikir jika saya tidak berubah dan masih berada di zona nyaman seperti ini bagaimana saya bisa berkembang dan membangun banyak jaringan.</p>
-        <p>Sekitar Agustus 2022 saya ditawari oleh seorang teman, namanya Tere. Ia berkata &ldquo;Dira, ayo gabung dengan KOMPAK, seru loh bisa banyak belajar hal dan bisa menjadi pengalaman berorganisasi bareng kaum muda di Batam. &ldquo;KOMPAK Itu apa sih Tere aku masih bingung,&rdquo; tanyaku kepada Tere. KOMPAK itu adalah komunitas orang muda anti perdagangan orang dan eksploitasi seksual anak, di sana kita akan belajar tentang isu-isu perdagangan orang atau biasa disebut TPPO dan juga bisa belajar bareng sama orang-orang hebat lainnya. Sejenak saya berpikir, dan mulai tertarik untuk bergabung bersama KOMPAK, apakah ini langkah awal saya untuk bisa bangkit dari kemalasan, mencoba untuk berkomitmen dan memiliki tanggung jawab pada diri sendiri. Akhirnya saya memutuskan bergabung dengan KOMPAK pada bulan berikutnya. Saya aktif menjadi anggota dan mengikuti kegiatan yang sangat bermanfaat bagi saya. Banyak belajar tentang hak-hak sebagai manusia bagaimana proses, cara dan tujuan dari tindak pidana perdagangan orang (TPPO) dan masih banyak ilmu yang didapat dari KOMPAK pada saat itu. Saya mulai banyak mengikuti kegiatan &ndash; kegiatan positif lainnya.</p>
-        <p>Meskipun awalnya sulit, tetapi jika kita berusaha untuk belajar, tidak ada kata untuk tidak bisa.</p>
+        <p>In the stillness of the evening, I scrolled through old photos that reminded me of important moments in life. I remembered the time very well. It was during the COVID-19 pandemic, yet my soul still hoped to be a high school student with all my heart. Those precious moments that were taken away made me lazy and I closed myself off from reaching out to the outside world. This also continued when I entered college, in 2021.&nbsp;</p>
         <p>
-          <blockquote>Seiring berjalannya waktu, sedikit banyaknya saya mulai merasakan perubahan. Dari diri yang begitu tertutup, sekarang bisa jauh lebih baik.</blockquote>
+          <blockquote>While everyone had lots of friends and relations, I was alone with my hopes of achieving my dreams. I want to be a person who is useful to other people.</blockquote>
         </p>
+        <p>Being able to share knowledge is a manifestation of my dream of becoming a lecturer, or a teacher. I thought, if I didn&rsquo;t change and was still in my comfort zone, how could I develop and build my network?</p>
+        <p>Around August 2022, a friend, Tere, encouraged me: &ldquo;Dira, come on, join KOMPAK. It&rsquo;s fun, you can learn a lot of things and it can be an experience in organizing with young people in Batam.&rdquo; I asked her, &ldquo;KOMPAK&hellip; What is that, Tere? I have no idea.&rdquo; KOMPAK is a community of young people aiming to combat human trafficking and child sexual exploitation. As a member, we can learn about issues of human trafficking, and learn together with other great people, she explained. I gave it a thought for a moment, and began to be interested in joining.&nbsp; I thought this could be my first step in overcoming my laziness, trying to be committed, and taking responsibility for myself. Finally, I decided to join KOMPAK the following month.&nbsp;</p>
+        <p>I am now an active member and take part in activities that are very beneficial for me. I have learned a lot about human rights, and the processes, methods and objectives of the criminal act of human trafficking. I have gained a lot of knowledge from KOMPAK and have started to take part in many other positive activities.</p>
+        <p>Even though it is difficult at first, if we try to learn, there is no such thing as the inability to do something. As time went by, I began to feel changes. From being so reclusive, now I am much better. Being here in Jakarta, and participating in the Safe and Fair activity programme with UN Women, is a process of change for me. I am building writing habits and shaping myself and my character to be better in the future. I will write more actively, so I can tell my life&rsquo;s journey in written form. I hope that by writing, I can benefit many people and spread the knowledge I have gained.</p>
         <p>
-          Berdirinya saya di sini di Jakarta, mengikuti program kegiatan
-          <em>Safe and Fair</em> bersama UN Women merupakan salah satu proses perubahan dari diri saya, membangun kebiasaan menulis dan membentuk diri maupun karakter saya supaya menjadi lebih baik di masa yang akan datang. Saya akan lebih giat menulis agar bisa menceritakan perjalanan hidup saya dalam bentuk tulisan. Harapan saya semoga dengan menulis saya dapat memberikan manfaat kepada banyak orang dan menyebarkan pengetahuan yang saya miliki.
-        </p>
-        <p>
-          <em>Dira Septiani, Bendahara di Komunitas Orang Muda Anti Perdagangan Orang dan Eksploitasi Seksual Anak (KOMPAK) Batam</em>
+          <em>Dira Septiani, Treasurer at the Youth Community Against Human Trafficking and Sexual Exploitation of Children (KOMPAK), Batam.</em>
         </p>
       </div>
     ),
